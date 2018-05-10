4f1891e1d9fa7e7a4a73510c59c3f704d7e451b9
Ivan Osadchiy <ivan.osadchii@gmail.com>
2018-05-10 19:18:13 +0300

    Enhance the dummy test

--- spec/system/appointments_spec.rb
+++ spec/system/appointments_spec.rb
@@ -1,6 +1,10 @@
-RSpec.describe "Appointments", js: true do
+RSpec.describe "Appointments" do
   it "dummy case" do
     visit "/appointments"
-    expect(page).to have_content "Appointments"
+    expect(page).to have_content "Appointments", "No results"
+
+    Appointment.create(title: "Appointment 1")
+    visit "/appointments"
+    expect(page).to have_content "Appointment 1"
   end
 end
