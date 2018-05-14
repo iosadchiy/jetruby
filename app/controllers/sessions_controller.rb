1baf0921a4a3a68bb9008cb33721e918e200acc6
Ivan Osadchiy <ivan.osadchii@gmail.com>
2018-05-14 20:53:45 +0300

    Show the sign in button only when not signed in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
   def create
+    reset_session
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
     redirect_to root_path
   end
 
   def destroy
+    reset_session
     session[:user_id] = nil
     redirect_to root_path
   end
